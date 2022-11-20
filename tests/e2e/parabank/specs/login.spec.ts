e5eb187475c00fbb46f84be31c4c166b99cab662
Adebowale Otulana <otulana.adebowale@gmail.com>
2022-11-20 11:31:18 -0700

    add prettier to project

--- tests/e2e/parabank/specs/login.spec.ts
+++ tests/e2e/parabank/specs/login.spec.ts
@@ -2,10 +2,10 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 
 test('homepage has title and links to intro page', async ({ page }) => {
-  const username = "john";
-  const password = "demo";
+  const username = 'john';
+  const password = 'demo';
 
   const loginPage = new LoginPage(page);
   await loginPage.goToLoginPage();
   await loginPage.login(username, password);
-})
+});
