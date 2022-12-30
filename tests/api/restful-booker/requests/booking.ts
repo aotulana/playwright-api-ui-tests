05f5bbc03609f2da93e930f1d56016bea1f82f6f
Adebowale Otulana <otulana.adebowale@gmail.com>
2022-12-30 09:56:36 -0700

    created booking tests

---
+++ tests/api/restful-booker/requests/booking.ts
@@ -0,0 +1,15 @@
+import { APIRequestContext } from '@playwright/test';
+
+export const getBookingIdsRequest = async (request: APIRequestContext) => {
+  return await request.get('/booking');
+};
+
+export const getFirstBookingIds = async (request: APIRequestContext) => {
+  return await getBookingIdsRequest(request).then(
+    async (response) => await response.json()
+  );
+};
+
+export const getBooking = async (request: APIRequestContext, id: number) => {
+  return await request.get(`/booking/${id}`);
+};
