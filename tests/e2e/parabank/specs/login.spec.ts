f7c17258e119853ab2fdcb7023a6bd969ff6c9be
Adebowale Otulana <otulana.adebowale@gmail.com>
2023-05-19 21:58:31 -0600

    chore: rename login page and refactor test methods

--- tests/e2e/parabank/specs/login.spec.ts
+++ tests/e2e/parabank/specs/login.spec.ts
@@ -1,21 +1,23 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/login-page';
+import { LoginPage } from '../pages/login.page';
 
-test('user with valid credentials should be able to login', async ({
-  page,
-}) => {
-  const username = 'john';
-  const password = 'demo';
+test.describe('Login', () => {
+  test('should be able to login user with valid credentials ', async ({
+    page,
+  }) => {
+    const username = 'john';
+    const password = 'demo';
 
-  const loginPage = new LoginPage(page);
-  await loginPage.goToLoginPage();
-  await loginPage.login(username, password);
-  expect(loginPage.welcomeText).toBeTruthy();
-});
+    const loginPage = new LoginPage(page);
+    await loginPage.goToLoginPage();
+    await loginPage.login(username, password);
+    expect(loginPage.welcomeText).toBeTruthy();
+  });
 
-test('new user should be able to register', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goToLoginPage();
-  await loginPage.registerNewUser();
-  expect(loginPage.successfulSignupText).toBeTruthy();
+  test('should be able to register new user ', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goToLoginPage();
+    await loginPage.registerNewUser();
+    expect(loginPage.successfulSignupText).toBeTruthy();
+  });
 });
